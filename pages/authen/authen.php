118b7fde4dc39037bf16bc6b9e7d3a56008514d0
Hmiddouch Abdessadek <abdessadekhm@gmail.com>
2024-04-30 16:54:04 +0100

    Finish Log In Page

---
+++ pages/authen/authen.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="authen.css">
+</head>
+
+<body>
+    <form class='log_form' action="" method="post">
+        <h1>Log In</h1>
+        <div class='input_container'>
+            <label for="username">Nom d'utilisateur</label>
+            <input placeholder="Nom d'utilisateur" class='input' type="text">
+        </div>
+        <div class='input_container'>
+            <label for="password">Mot De Passe</label>
+            <input placeholder='Mot de passe' class='input' type="password">
+        </div>
+        <div class='btn_container'>
+            <input class='submit_btn' type="submit" value="Submit">
+        </div>
+    </form>
+</body>
+
+</html>
