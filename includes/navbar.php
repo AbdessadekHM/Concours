15a2acc13bfc7ffc362d4f2da035c05b49d49328
Hmiddouch Abdessadek <abdessadekhm@gmail.com>
2024-05-04 13:10:37 +0100

    Add Navbar

---
+++ includes/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary ps-5 pe-5" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Ensa</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="../pages/Home/home.php">Home</a>
+                <a class="nav-link" href="../pages/Register/register.php">Register</a>
+                <a class="nav-link" href="../pages/LogIn/logIn.php">Log In</a>
+                <a class="nav-link disabled d-none" aria-disabled="true">Dashboard</a>
+            </div>
+        </div>
+    </div>
+</nav>
