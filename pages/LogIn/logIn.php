3382c7ca4268ffbd5a6d36f15b6fb3f06a637260
Hmiddouch Abdessadek <abdessadekhm@gmail.com>
2024-05-05 00:41:35 +0100

    Add log in page

---
+++ pages/LogIn/logIn.php
@@ -0,0 +1,46 @@
+<?php 
+$page_title = 'Log In'; 
+?>
+<?php include('../../includes/header.php') ?>
+<?php include('../../includes/navbar.php') ?>
+<div class="py-5">
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card shadow">
+                    <div class="card-header text-center">
+                        Log In
+                    </div>
+                    <div class="card-body">
+                        <form class="row justify-content-around">
+
+                            <div class="form-group pb-4">
+                                <label class='pb-3' for="log">Nom d'utilisateur</label>
+                                <input type="text" class="form-control" id="log" name="log">
+                            </div>
+                            <div class="form-group pb-4">
+                                <label class='pb-3' for="mdp">Mot de passe</label>
+                                <input type="password" class="form-control" id="mdp" name="mdp">
+                            </div>
+                            <div class="row form-group pt-5 pe-3 justify-content-end">
+                                <button class=" col-md-2 btn btn-primary " type="submit">
+                                    Submit
+                                </button>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+</div>
+
+
+
+
+
+
+<?php include('../../includes/footer.php') ?>
