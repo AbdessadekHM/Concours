b8fe7d8d0ea2cb70e547aa8b77d7e012f5a70b26
Hmiddouch Abdessadek <abdessadekhm@gmail.com>
2024-05-05 22:36:06 +0100

    make store function dynamic

--- utils/store.php
+++ utils/store.php
@@ -1,12 +1,17 @@
 <?php
-function Store($file,$type,$name){
-    $target_dir = "../../uploads/$type/";
+function Store($file,$type,$name,$tmp){
+    if($tmp){
+        $target_dir = "../../tmp_uploads/$type/";
+    }else{
+        $target_dir = "../../uploads/$type/";
+    }
     $extension = pathinfo(basename($file['name']), PATHINFO_EXTENSION);
     $target_file = $target_dir . $name . '.' .$extension ;
     try {
         echo "</br>$target_file</br>";
+        var_dump(move_uploaded_file($file['tmp_name'],$target_file));
         move_uploaded_file($file['tmp_name'],$target_file);
-        echo 'fine';
+       
     } catch (Exception $e) {
         echo 'error' . $e->getMessage();
     }
